c85fb10ce3fc554c967a055ef98b5a7e0d3b9b48
Leandra Bordin <leandra_bordin@hotmail.comgit config --global user.email leandra_bordin@hotmail.com>
2026-05-08 10:41:27 -0300

    commit inicial do projeto

---
+++ src/test/java/com/leandra/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandra.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
